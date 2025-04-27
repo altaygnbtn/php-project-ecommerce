e8a143d60acad4863ebc124cd5f1005024443358
altaygnbtn <altaygunbatan@hotmail.com>
2025-04-27 07:48:44 +0300

    implemented file uploading to be displayed

--- save_product.php
+++ save_product.php
@@ -11,12 +11,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (!is_dir($target_dir)) {
         mkdir($target_dir, 0777, true);
     }
-    $image_name = basename($_FILES["image"]["name"]);
+    $image_name = basename($_FILES["image"]["name"]); //stores the original filename from the client
     $target_file = $target_dir . $image_name;
 
-    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) { //move the temporary stored file to the specified location
 
-        $stmt = $mysqli->prepare("INSERT INTO products (name, description, price, image) VALUES (?, ?, ?, ?)");
+        $stmt = $mysqli->prepare(query: "INSERT INTO products (name, description, price, image) VALUES (?, ?, ?, ?)");
         $stmt->bind_param("ssds", $name, $desc, $price, $target_file);
         $stmt->execute();
         echo "Product added successfully!<br>";
